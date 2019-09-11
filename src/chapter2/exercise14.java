4f718e42c33b8236baa03c4071ba4051eb6621ae
awtpi314 <52684858+awtpi314@users.noreply.github.com>
2019-09-11 09:02:44 -0500

    added 2.14

--- src/chapter2/exercise14.java
+++ src/chapter2/exercise14.java
@@ -2,27 +2,64 @@ package chapter2;
 
 import java.util.Scanner;
 
+/**
+ * exercise14
+ * Alexander Taylor
+ * TPS Java
+ * Mr. Yonts
+ *
+ * This class calculates BMI based on height and weight
+ * The user is prompted for weight in pounds and height
+ * in inches. The class then converts the pounds and inches
+ * to kilograms and meters. The BMI is then calculated and
+ * output to the console.
+ */
 public class exercise14 {
     public static void main(String[] args) {
+        // Declare the input scanner
         Scanner sc = new Scanner(System.in);
+
+        // The variables used to store information
+        // Initially set to -1 for use in input
         double pounds = -1;
         double inches = -1;
         double kilograms;
         double meters;
 
+        // Prompt the user
         System.out.print("Enter weight in pounds: ");
-        if (sc.hasNextDouble()) {
-            pounds = sc.nextDouble();
+        // Continue looping until a correct value is entered
+        while (pounds <= 0) {
+            // Get the next double from the scanner
+            if (sc.hasNextDouble()) {
+                pounds = sc.nextDouble();
+            } else {
+                // Clear the input stream so the next call doesn't fail
+                sc.next();
+                System.out.println("Invalid input. Enter weight in pounds: ");
+            }
         }
 
+        // Prompt the user
         System.out.print("Enter height in inches: ");
-        if (sc.hasNextDouble()) {
-            inches = sc.nextDouble();
+        // Continue looping until a correct value is entered
+        while(inches <= 0) {
+            // Get the next double from the scanner
+            if (sc.hasNextDouble()) {
+                inches = sc.nextDouble();
+            } else {
+                // Clear the input stream so the next call doesn't fail
+                sc.next();
+                System.out.println("Invalid input. Enter height in inches: ");
+            }
         }
 
-        kilograms = pounds / 0.45359237;
-        meters = inches / 0.0254;
+        // Convert the pounds to kilograms
+        kilograms = pounds * 0.45359237;
+        // Convert the inches to meters
+        meters = inches * 0.0254;
 
+        // Calculate and print the BMI
         System.out.printf("BMI is %.4f", (kilograms / Math.pow(meters, 2)));
     }
 }
