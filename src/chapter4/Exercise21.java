ecc35388197e3608ccada6d0c32dbb117684a4ba
awtpi314 <52684858+awtpi314@users.noreply.github.com>
2019-10-28 09:26:17 -0500

    Changed Exercise 4.21

--- src/chapter4/Exercise21.java
+++ src/chapter4/Exercise21.java
@@ -1,6 +1,7 @@
 package chapter4;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Exercise21 {
     public static void main(String[] args) {
@@ -8,11 +9,10 @@ public class Exercise21 {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         sc.close();
-        String[] inDiv = input.split("-");
-        if (inDiv[0].length() == 3 && inDiv[1].length() == 2 && inDiv[2].length() == 4) {
-            System.out.println(input + " is a valid Social Security number.");
+        if (Pattern.matches("[0-9]{3}-[0-9]{2}-[0-9]{4}", input)) {
+            System.out.println(input + " is a Social Security number.");
         } else {
-            System.out.println(input + " is an invalid Social Security number.");
+            System.out.println(input + " is not a Social Security number.");
         }
     }
 }
