2b54132d24ad69dbcef8f4b172aea2302e8ef7e0
Alexander Taylor <52684858+awtpi314@users.noreply.github.com>
2020-01-27 08:46:08 -0600

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,1 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
-public class Test {
-    public static void main(String[] args) {
-        BigDecimal a = new BigDecimal("3.0");
-        BigDecimal b = new BigDecimal("2.0");
-        System.out.println(a.divide(b, RoundingMode.HALF_EVEN).toString());
-    }
-}
+import java.util.Scanner; public class Wk19 { public static void main(String[] args) { Scanner keyboard = new Scanner(System.in); System.out.print("Enter 10 double values: "); double[] array = new double[10]; for (int i = 0; i < array.length; i++) { array[i] = keyboard.nextDouble(); } keyboard.close(); sort(array); System.out.println(java.util.Arrays.toString(array)); } public static void sort(double[] v) { for (int i = v.length - 1; i > 0; i--) { for (int j = i - 1; j > -1; j--) { if (v[j] > v[i]) { double t = v[j]; v[j] = v[i]; v[i] = t; } } } } }
