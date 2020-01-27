55b0240c6f1bb688740b49f545253367cec46fe9
awtpi314 <52684858+awtpi314@users.noreply.github.com>
2020-01-27 08:47:28 -0600

    Changed Test.java formatting

--- src/Test.java
+++ src/Test.java
@@ -1,1 +1,27 @@
-import java.util.Scanner; public class Wk19 { public static void main(String[] args) { Scanner keyboard = new Scanner(System.in); System.out.print("Enter 10 double values: "); double[] array = new double[10]; for (int i = 0; i < array.length; i++) { array[i] = keyboard.nextDouble(); } keyboard.close(); sort(array); System.out.println(java.util.Arrays.toString(array)); } public static void sort(double[] v) { for (int i = v.length - 1; i > 0; i--) { for (int j = i - 1; j > -1; j--) { if (v[j] > v[i]) { double t = v[j]; v[j] = v[i]; v[i] = t; } } } } }
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter 10 double values: ");
+        double[] array = new double[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = keyboard.nextDouble();
+        }
+        keyboard.close();
+        sort(array);
+        System.out.println(java.util.Arrays.toString(array));
+    }
+
+    public static void sort(double[] v) {
+        for (int i = v.length - 1; i > 0; i--) {
+            for (int j = i - 1; j > -1; j--) {
+                if (v[j] > v[i]) {
+                    double t = v[j];
+                    v[j] = v[i];
+                    v[i] = t;
+                }
+            }
+        }
+    }
+}
